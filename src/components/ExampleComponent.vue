1b65d8c0022ace8156e6ef8ba7e7d2496f645903
Jesuskor <thejesuskor@gmail.com>
2025-05-20 17:03:50 -0600

    v1.1.0

---
+++ src/components/ExampleComponent.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+
+const status = ref(false)
+const toggle = () => {
+  status.value = !status.value
+}
+</script>
+
+<template>
+  <div>
+    <p>Estado: {{ status }}</p>
+    <button @click="toggle">Toggle</button>
+  </div>
+</template>
